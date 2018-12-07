d681e5077fc6963093158ff11a6d33615954a7f5
JackVachon <36710461+JackVachon@users.noreply.github.com>
2018-12-07 12:45:55 -0500

    Version1
    Version1 App Works, needs redesigning.

---
+++ app/src/main/java/com/example/vacho/mobileappcatfacts/ColorWheel.java
@@ -0,0 +1,26 @@
+package com.example.vacho.mobileappcatfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+public class ColorWheel {
+
+    private String[] colors = {
+            "#397367",
+            "#5DA399",
+            "#63CCCA",
+            "#C4AF9A",
+            "#CB04A5",
+            "#E0CA3C",
+            "#F7BFB4",
+            "#FB9F89",
+            "#FFB86F"
+    };
+
+    int getColor() {
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(colors.length);
+        return Color.parseColor(colors[randomNumber]);
+    }
+}
---
+++ app/src/main/java/com/example/vacho/mobileappcatfacts/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.vacho.mobileappcatfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+
+    //Declare our view variables
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_facts);
+
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = factBook.getFact();
+                int color = colorWheel.getColor();
+                factTextView.setText(fact);
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+    }
+}
